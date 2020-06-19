e98731e58a169627e46be2fa51ed6f4c490f87ef
phil65 <philipptemminghoff@googlemail.com>
2020-06-19 23:44:28 +0200

    Bump version: 0.2.2 → 0.2.3

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/phil65/pandastools",
-    version="0.2.2",
+    version="0.2.3",
     zip_safe=False,
     # cmdclass={"build_sphinx": BuildDoc}
 )
