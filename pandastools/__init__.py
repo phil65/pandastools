012246b49340e17a2559ceb3bde0e6538ed0e924
phil65 <philipptemminghoff@googlemail.com>
2020-06-19 22:04:58 +0200

    cleanup

--- pandastools/__init__.py
+++ pandastools/__init__.py
@@ -5,7 +5,7 @@
 from . import accessors
 
 __author__ = """Philipp Temminghoff"""
-__email__ = 'phil65@kodi.tv'
-__version__ = '0.2.0'
+__email__ = "phil65@kodi.tv"
+__version__ = "0.2.0"
 
 __all__ = ["accessors", "utils"]
